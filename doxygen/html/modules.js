2e5fdc55aeb150ebe64a378b788267113ca88537
yaoqiuguo <yao.yqg@alibaba-inc.com>
2021-04-02 11:33:57 +0800

    for test
    Change-Id: Iefd8d1247b4b59f72ca1e064b49080f1bfb2c689

---
+++ doxygen/html/modules.js
@@ -0,0 +1,20 @@
+var modules =
+[
+    [ "a2sa", "group__a2sa__api.html", "group__a2sa__api" ],
+    [ "aiagent", "group__aiagent__aos__api.html", "group__aiagent__aos__api" ],
+    [ "cli", "group__cli__aos__api.html", "group__cli__aos__api" ],
+    [ "debug", "group__debug__aos__api.html", "group__debug__aos__api" ],
+    [ "http", "group__aos__httpclient__api.html", "group__aos__httpclient__api" ],
+    [ "init", "group__init__aos__api.html", "group__init__aos__api" ],
+    [ "kv", "group__kv__aos__api.html", "group__kv__aos__api" ],
+    [ "netmgr", "group__netmgr__api.html", "group__netmgr__api" ],
+    [ "sntp", "group__sntp__api.html", "group__sntp__api" ],
+    [ "ramfs", "group__ramfs__aos__api.html", "group__ramfs__aos__api" ],
+    [ "ucloud_ai", "group__ucloud__ai__aos__api.html", "group__ucloud__ai__aos__api" ],
+    [ "ucamera", "group__ucamera__aos__api.html", "group__ucamera__aos__api" ],
+    [ "udisplay", "group__udisplay__aos__api.html", "group__udisplay__aos__api" ],
+    [ "uvoice", "group__uvoice__aos__api.html", "group__uvoice__aos__api" ],
+    [ "mbmaster", "group__aos__mbmaster.html", "group__aos__mbmaster" ],
+    [ "内核", "group__aos__kernel.html", "group__aos__kernel" ],
+    [ "ulog", "group__aos__ulog.html", "group__aos__ulog" ]
+];
---
+++ doxygen/html/peripheral.js
@@ -0,0 +1,193 @@
+var peripheral =
+[
+    [ "adc", "adc.html", [
+      [ "概述", "adc.html#autotoc_md1310", [
+        [ "版权信息", "adc.html#autotoc_md1311", null ],
+        [ "目录结构", "adc.html#autotoc_md1312", null ],
+        [ "依赖组件", "adc.html#autotoc_md1313", null ]
+      ] ],
+      [ "常用配置", "adc.html#autotoc_md1314", null ],
+      [ "API说明", "adc.html#autotoc_md1315", null ],
+      [ "使用示例", "adc.html#autotoc_md1316", [
+        [ "添加示例代码", "adc.html#autotoc_md1317", null ],
+        [ "添加adc组件", "adc.html#autotoc_md1318", null ],
+        [ "编译", "adc.html#autotoc_md1319", null ],
+        [ "烧录固件", "adc.html#autotoc_md1320", null ],
+        [ "KV示例测试", "adc.html#autotoc_md1321", null ],
+        [ "关键日志", "adc.html#autotoc_md1322", null ]
+      ] ],
+      [ "注意事项", "adc.html#autotoc_md1323", null ],
+      [ "FAQ", "adc.html#autotoc_md1324", null ]
+    ] ],
+    [ "flash", "flash.html", [
+      [ "概述", "flash.html#autotoc_md1325", [
+        [ "分区定义", "flash.html#autotoc_md1326", null ],
+        [ "版权信息", "flash.html#autotoc_md1327", null ],
+        [ "目录结构", "flash.html#autotoc_md1328", null ],
+        [ "依赖组件", "flash.html#autotoc_md1329", null ]
+      ] ],
+      [ "常用配置", "flash.html#autotoc_md1330", null ],
+      [ "API说明", "flash.html#autotoc_md1331", null ],
+      [ "使用示例", "flash.html#autotoc_md1332", [
+        [ "添加示例代码", "flash.html#autotoc_md1333", null ],
+        [ "添加flash组件", "flash.html#autotoc_md1334", null ],
+        [ "编译", "flash.html#autotoc_md1335", null ],
+        [ "烧录固件", "flash.html#autotoc_md1336", null ],
+        [ "KV示例测试", "flash.html#autotoc_md1337", null ],
+        [ "关键日志", "flash.html#autotoc_md1338", null ]
+      ] ],
+      [ "注意事项", "flash.html#autotoc_md1339", null ],
+      [ "FAQ", "flash.html#autotoc_md1340", null ]
+    ] ],
+    [ "GPIO", "gpio.html", [
+      [ "概述", "gpio.html#autotoc_md1341", [
+        [ "版权说明", "gpio.html#autotoc_md1342", null ],
+        [ "目录结构", "gpio.html#autotoc_md1343", null ],
+        [ "依赖组件", "gpio.html#autotoc_md1344", null ]
+      ] ],
+      [ "常用配置", "gpio.html#autotoc_md1345", null ],
+      [ "API说明", "gpio.html#autotoc_md1346", [
+        [ "打开", "gpio.html#autotoc_md1347", null ],
+        [ "关闭", "gpio.html#autotoc_md1348", null ],
+        [ "配置输入属性并获取输入电平", "gpio.html#autotoc_md1349", null ],
+        [ "配置输出属性并获取输出电平", "gpio.html#autotoc_md1350", null ],
+        [ "配置中断", "gpio.html#autotoc_md1351", null ]
+      ] ],
+      [ "使用示例", "gpio.html#autotoc_md1352", [
+        [ "添加示例代码", "gpio.html#autotoc_md1353", null ],
+        [ "添加组件", "gpio.html#autotoc_md1354", null ],
+        [ "编译", "gpio.html#autotoc_md1355", null ],
+        [ "烧录固件", "gpio.html#autotoc_md1356", null ],
+        [ "示例测试", "gpio.html#autotoc_md1357", null ],
+        [ "关键日志", "gpio.html#autotoc_md1358", null ]
+      ] ],
+      [ "注意事项", "gpio.html#autotoc_md1359", null ],
+      [ "FAQ", "gpio.html#autotoc_md1360", null ]
+    ] ],
+    [ "i2c", "i2c.html", [
+      [ "概述", "i2c.html#autotoc_md1361", [
+        [ "版权信息", "i2c.html#autotoc_md1362", null ],
+        [ "目录结构", "i2c.html#autotoc_md1363", null ],
+        [ "依赖组件", "i2c.html#autotoc_md1364", null ]
+      ] ],
+      [ "常用配置", "i2c.html#autotoc_md1365", null ],
+      [ "API说明", "i2c.html#autotoc_md1366", null ],
+      [ "使用示例", "i2c.html#autotoc_md1367", [
+        [ "添加示例代码", "i2c.html#autotoc_md1368", null ],
+        [ "添加I2C组件", "i2c.html#autotoc_md1369", null ],
+        [ "编译", "i2c.html#autotoc_md1370", null ],
+        [ "烧录固件", "i2c.html#autotoc_md1371", null ],
+        [ "I2C示例测试", "i2c.html#autotoc_md1372", null ],
+        [ "关键日志", "i2c.html#autotoc_md1373", null ],
+        [ "关键日志", "i2c.html#autotoc_md1374", null ]
+      ] ],
+      [ "注意事项", "i2c.html#autotoc_md1375", null ],
+      [ "FAQ", "i2c.html#autotoc_md1376", null ]
+    ] ],
+    [ "pwm", "pwm.html", [
+      [ "概述", "pwm.html#autotoc_md1377", [
+        [ "版权信息", "pwm.html#autotoc_md1378", null ],
+        [ "目录结构", "pwm.html#autotoc_md1379", null ],
+        [ "依赖组件", "pwm.html#autotoc_md1380", null ]
+      ] ],
+      [ "常用配置", "pwm.html#autotoc_md1381", null ],
+      [ "API说明", "pwm.html#autotoc_md1382", null ],
+      [ "使用示例", "pwm.html#autotoc_md1383", [
+        [ "添加示例代码", "pwm.html#autotoc_md1384", null ],
+        [ "添加PWM组件", "pwm.html#autotoc_md1385", null ],
+        [ "编译", "pwm.html#autotoc_md1386", null ],
+        [ "烧录固件", "pwm.html#autotoc_md1387", null ],
+        [ "PWM示例测试", "pwm.html#autotoc_md1388", null ],
+        [ "关键日志", "pwm.html#autotoc_md1389", null ]
+      ] ],
+      [ "注意事项", "pwm.html#autotoc_md1390", null ],
+      [ "FAQ", "pwm.html#autotoc_md1391", null ]
+    ] ],
+    [ "spi", "spi.html", [
+      [ "概述", "spi.html#autotoc_md1392", [
+        [ "版权信息", "spi.html#autotoc_md1393", null ],
+        [ "目录结构", "spi.html#autotoc_md1394", null ],
+        [ "依赖组件", "spi.html#autotoc_md1395", null ]
+      ] ],
+      [ "常用配置", "spi.html#autotoc_md1396", null ],
+      [ "API说明", "spi.html#autotoc_md1397", null ],
+      [ "使用示例", "spi.html#autotoc_md1398", [
+        [ "添加示例代码", "spi.html#autotoc_md1399", null ],
+        [ "添加spi组件", "spi.html#autotoc_md1400", null ],
+        [ "编译", "spi.html#autotoc_md1401", null ],
+        [ "烧录固件", "spi.html#autotoc_md1402", null ],
+        [ "SPI示例测试", "spi.html#autotoc_md1403", null ],
+        [ "关键日志", "spi.html#autotoc_md1404", null ]
+      ] ],
+      [ "注意事项", "spi.html#autotoc_md1405", null ],
+      [ "FAQ", "spi.html#autotoc_md1406", null ]
+    ] ],
+    [ "timer", "timer.html", [
+      [ "概述", "timer.html#autotoc_md1407", [
+        [ "版权信息", "timer.html#autotoc_md1408", null ],
+        [ "目录结构", "timer.html#autotoc_md1409", null ],
+        [ "依赖组件", "timer.html#autotoc_md1410", null ]
+      ] ],
+      [ "常用配置", "timer.html#autotoc_md1411", null ],
+      [ "API说明", "timer.html#autotoc_md1412", null ],
+      [ "使用示例", "timer.html#autotoc_md1413", [
+        [ "添加示例代码", "timer.html#autotoc_md1414", null ],
+        [ "添加timer组件", "timer.html#autotoc_md1415", null ],
+        [ "编译", "timer.html#autotoc_md1416", null ],
+        [ "烧录固件", "timer.html#autotoc_md1417", null ],
+        [ "Timer示例测试", "timer.html#autotoc_md1418", null ],
+        [ "关键日志", "timer.html#autotoc_md1419", null ]
+      ] ],
+      [ "注意事项", "timer.html#autotoc_md1420", null ],
+      [ "FAQ", "timer.html#autotoc_md1421", null ]
+    ] ],
+    [ "UART", "uart.html", [
+      [ "概述", "uart.html#autotoc_md1422", [
+        [ "版权说明", "uart.html#autotoc_md1423", null ],
+        [ "目录结构", "uart.html#autotoc_md1424", null ],
+        [ "依赖组件", "uart.html#autotoc_md1425", null ]
+      ] ],
+      [ "常用配置", "uart.html#autotoc_md1426", null ],
+      [ "API说明", "uart.html#autotoc_md1427", [
+        [ "打开", "uart.html#autotoc_md1428", null ],
+        [ "关闭", "uart.html#autotoc_md1429", null ],
+        [ "接收数据", "uart.html#autotoc_md1430", null ],
+        [ "发送数据", "uart.html#autotoc_md1431", null ],
+        [ "查询IO事件", "uart.html#autotoc_md1432", null ],
+        [ "设置属性", "uart.html#autotoc_md1433", [
+          [ "波特率", "uart.html#autotoc_md1434", null ],
+          [ "数据位数", "uart.html#autotoc_md1435", null ],
+          [ "校验方式", "uart.html#autotoc_md1436", null ]
+        ] ]
+      ] ],
+      [ "使用示例", "uart.html#autotoc_md1437", [
+        [ "添加示例代码", "uart.html#autotoc_md1438", null ],
+        [ "添加组件", "uart.html#autotoc_md1439", null ],
+        [ "编译", "uart.html#autotoc_md1440", null ],
+        [ "烧录固件", "uart.html#autotoc_md1441", null ],
+        [ "示例测试", "uart.html#autotoc_md1442", null ],
+        [ "关键日志", "uart.html#autotoc_md1443", null ]
+      ] ],
+      [ "注意事项", "uart.html#autotoc_md1444", null ],
+      [ "FAQ", "uart.html#autotoc_md1445", null ]
+    ] ],
+    [ "watchdog", "watchdog.html", [
+      [ "概述", "watchdog.html#autotoc_md1446", [
+        [ "版权信息", "watchdog.html#autotoc_md1447", null ],
+        [ "目录结构", "watchdog.html#autotoc_md1448", null ],
+        [ "依赖组件", "watchdog.html#autotoc_md1449", null ]
+      ] ],
+      [ "常用配置", "watchdog.html#autotoc_md1450", null ],
+      [ "API说明", "watchdog.html#autotoc_md1451", null ],
+      [ "使用示例", "watchdog.html#autotoc_md1452", [
+        [ "添加示例代码", "watchdog.html#autotoc_md1453", null ],
+        [ "添加WATCHDOG组件", "watchdog.html#autotoc_md1454", null ],
+        [ "编译", "watchdog.html#autotoc_md1455", null ],
+        [ "烧录固件", "watchdog.html#autotoc_md1456", null ],
+        [ "WATCHDOG示例测试", "watchdog.html#autotoc_md1457", null ],
+        [ "关键日志", "watchdog.html#autotoc_md1458", null ]
+      ] ],
+      [ "注意事项", "watchdog.html#autotoc_md1459", null ],
+      [ "FAQ", "watchdog.html#autotoc_md1460", null ]
+    ] ]
+];
